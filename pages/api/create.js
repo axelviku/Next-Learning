eef031681ea60b2dd0d16d033c13b79c0ee404d0
Vikrant Kumar <axelice89@gmail.com>
2024-05-23 18:49:16 +0530

    Update create.js

--- pages/api/create.js
+++ pages/api/create.js
@@ -2,17 +2,28 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-        const { name, email,posts } = req.body;
-        const user = await prisma.user.create({
-            data: {
-                name,
-                email,
-               
-              },
-          
+        const { name, email, posts } = req.body;
+
+        const user = await prisma.user.findUnique({
+            where:{ email}
         });
-       return res.status(201).json(user);
+
+        if (user) {
+            return res.send({ Message: "This user is already register" })
+        } else {
+            const user = await prisma.user.create({
+                data: {
+                    name: name,
+                    email: email,
+                },
+
+            });
+            return res.json({message:"This user is succesfully register.",user});
+        }
+
+
     }
 }
